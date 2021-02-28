030d85673676995dc9b5b8a2de2af1088e119cfb
FedericoCalonge <calongefederico@gmail.com>
2021-02-28 15:21:50 -0300

    2-escala_app_mater_Sentinel terminado

--- 2-escala-app-master_Sentinel/src/main/java/ar/edu/undav/escalaapp/config/RedisConfig.java
+++ 2-escala-app-master_Sentinel/src/main/java/ar/edu/undav/escalaapp/config/RedisConfig.java
@@ -1,11 +1,7 @@
 package ar.edu.undav.escalaapp.config;
-import io.lettuce.core.sentinel.api.StatefulRedisSentinelConnection;
-import io.lettuce.core.sentinel.api.sync.RedisSentinelCommands;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -20,31 +16,13 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    //Anterior sin Sentinel (funciona):
-    /*
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("redis-master", 6379));
-    }
-    */
-
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
                 .master("undavmaster")
-                .sentinel("127.0.0.1", 26379);
+                .sentinel("redis-sentinel_1", 26379)
+                .sentinel("redis-sentinel_2", 26380)
+                .sentinel("redis-sentinel_3", 26381);
         return new LettuceConnectionFactory(sentinelConfig);
         }
 }
-
-    //Ejemplo original con Sentinel:
-    /*
-    @Bean
-    public RedisConnectionFactory lettuceConnectionFactory() {
-      RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-      .master("undavmaster")
-      .sentinel("127.0.0.1", 26579)
-      .sentinel("127.0.0.1", 26580);
-      return new LettuceConnectionFactory(sentinelConfig);
-    }
-    */
